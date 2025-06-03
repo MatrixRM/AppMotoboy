e801081ed678cc62a36ec12ad6c48b4c412189af
260297 <robertomanica@yahoo.com.br>
2025-06-03 13:40:46 -0300

    statics

--- escalas_motoboy/settings.py
+++ escalas_motoboy/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -111,3 +111,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # pasta que será usada no deploy
